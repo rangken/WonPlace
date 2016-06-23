1a7c439a3ebe625bbe7104ed035460606a2ba1be
young-k <young.k@daumkakao.com>
2016-06-24 01:29:18 +0900

    g

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -12,7 +12,9 @@ module Api
     end
 
     def index
-      @posts = Post.where(user_id: current_user.id).order(created_at: :desc)
+      count = params[:count] || 20
+      page = params[:page] || 1
+      @posts = Post.where(user_id: current_user.id).page(page).per(count).order(created_at: :desc)
     end
 
     def show
@@ -22,6 +24,7 @@ module Api
     def create
       count = Post.where('user_id = ? and created_at > ? and spot_id = ?', current_user.id, Time.now.midnight, params[:spot_id]).count
       if count >= 10
+        puts '하루 10개를 초과 했습니다.'
         render_success('하루 10개를 초과 했습니다')
         return
       end
